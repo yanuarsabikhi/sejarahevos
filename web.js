1bb28db46697a0197d5f7dd50de5cd8e15b71a3b
Yanuar Sabikhi <97417159+yanuarsabikhi@users.noreply.github.com>
2022-09-01 13:11:09 +0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,16 @@
+alert("Selamat Datang di Sejarah EVOS");
+
+function scrollEvos() {
+    var artikel = document.getElementById("evos");
+    artikel.scrollIntoView();
+}
+
+function scrollRoaster() {
+    var artikel = document.getElementById("roaster");
+    artikel.scrollIntoView();
+}
+
+function scrollM1() {
+    var artikel = document.getElementById("m1");
+    artikel.scrollIntoView();
+}
